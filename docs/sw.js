0e14aa1ef65a874304a813e7acf943d6656db327
count-null <70529195+count-null@users.noreply.github.com>
2021-04-09 11:32:29 -0400

    fix homepage show posts

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210407.1554';
+var cacheName = 'chirpy-20210409.1132';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
